6d9b320aea08ea3afb9a440bd34a45353b157166
Imashi Fernando <imashifernando20019@gmail.com>
2026-07-12 20:04:11 +0530

    Add PageController and organize templates

---
+++ campusswap/src/main/java/jar/controller/PageController.java
@@ -0,0 +1,23 @@
+package jar.controller; 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    
+    @GetMapping("/login.html")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register.html")
+    public String registerPage() {
+        return "register";
+    }
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
