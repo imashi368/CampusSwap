f00200765a36e8a3952b3e5b68d79d34a482d048
Imashi Fernando <imashifernando20019@gmail.com>
2026-07-14 16:11:33 +0530

    Fix controller forwarding

--- campusswap/src/main/java/jar/controller/HomeController.java
+++ campusswap/src/main/java/jar/controller/HomeController.java
@@ -1,4 +1,4 @@
-package jar.controller;  
+package jar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "forward:/index.html";
     }
 }
